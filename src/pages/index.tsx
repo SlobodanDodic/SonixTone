dc8abade565d024682576492883b9f2a25a209e9
SlobodanDodic <slobodandodic@gmail.com>
2023-07-13 19:41:05 +0200

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,8 +26,8 @@ export default function Home() {
             >
               <h3 className="text-2xl font-bold">First Steps →</h3>
               <div className="text-lg">
-                Just the basics - Everything you need to know to set up your
-                database and authentication.
+                Everything you need to know to set up your database and
+                authentication.
               </div>
             </Link>
             <Link
