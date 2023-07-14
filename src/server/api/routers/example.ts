747222e0fab1b01e1c06c58200f997a1aac31f82
SlobodanDodic <slobodandodic@gmail.com>
2023-07-14 11:43:38 +0200

    example change

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -6,19 +6,19 @@ import {
 } from "@/server/api/trpc";
 
 export const exampleRouter = createTRPCRouter({
-  hello: publicProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input.text}`,
-      };
-    }),
+  // hello: publicProcedure
+  //   .input(z.object({ text: z.string() }))
+  //   .query(({ input }) => {
+  //     return {
+  //       greeting: `Hello ${input.text}`,
+  //     };
+  //   }),
 
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.example.findMany();
-  }),
+  // getAll: publicProcedure.query(({ ctx }) => {
+  //   return ctx.prisma.example.findMany();
+  // }),
 
-  getSecretMessage: protectedProcedure.query(() => {
-    return "You are now logged!";
-  }),
+  // getSecretMessage: protectedProcedure.query(() => {
+  //   return "You are now logged!";
+  // }),
 });
