f888ee601fad7eb055f6bdba9bcdd13a38d19f07
SlobodanDodic <slobodandodic@gmail.com>
2023-07-30 19:32:58 +0200

    getServerSideProps

--- src/pages/add_guitar/index.tsx
+++ src/pages/add_guitar/index.tsx
@@ -3,9 +3,6 @@ import { useFormState } from "@/context/FormContext";
 import { Brand } from "@/components/AddGuitar/steps/Brand";
 import { Details } from "@/components/AddGuitar/steps/Details";
 import { Preview } from "@/components/AddGuitar/steps/Preview";
-// Auth
-import IsAuth from "@/utils/IsAuth";
-import { NextPage } from "next";
 
 function ActiveStepFormComponent() {
   const { step } = useFormState();
@@ -21,7 +18,7 @@ function ActiveStepFormComponent() {
   }
 }
 
-const AddGuitar: NextPage = () => {
+export default function AddGuitar() {
   return (
     <main className="relative flex h-screen w-screen flex-col items-center p-4 text-gray-600">
       <div className="absolute left-1/2 top-0 -z-20 hidden h-full w-screen max-w-xl -translate-x-1/2 transform justify-center bg-gradient-to-b from-transparent via-red-900 to-transparent sm:flex" />
@@ -31,6 +28,25 @@ const AddGuitar: NextPage = () => {
       </div>
     </main>
   );
-};
+}
 
-export default IsAuth(AddGuitar);
+// Redirect not logged-in user:
+import { type GetServerSidePropsContext } from "next";
+import { getServerAuthSession } from "@/server/auth";
+
+export async function getServerSideProps(ctx: GetServerSidePropsContext) {
+  const session = await getServerAuthSession(ctx);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
